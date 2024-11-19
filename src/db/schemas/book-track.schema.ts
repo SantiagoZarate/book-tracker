bbdf97875691122064c97f5b09e3acb9776bcc9c
SantiagoZarate <santiago2001zarate@gmail.com>
2024-11-19 01:15:48 -0300

    feat: add track and book schemas

--- src/db/schemas/book-track.schema.ts
+++ src/db/schemas/book-track.schema.ts
@@ -1,5 +1,41 @@
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { relations, sql } from "drizzle-orm";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { nanoid } from "nanoid";
 
-export const bookTrackSchema = sqliteTable("book_track", {
-  id: text("id"),
+export const bookSchema = sqliteTable("book", {
+  id: text("id")
+    .primaryKey()
+    .notNull()
+    .$defaultFn(() => nanoid()),
+  title: text("title").notNull(),
+  author: text("author"),
+  totalPages: integer("total_pages").notNull().default(1),
 });
+
+export const trackSchema = sqliteTable("track", {
+  id: text("id")
+    .primaryKey()
+    .notNull()
+    .$defaultFn(() => nanoid()),
+  pagesAlreadyRead: integer("pages_already_read").notNull().default(0),
+  isCompleted: integer("is_completed", { mode: "boolean" })
+    .notNull()
+    .default(false),
+  startedAt: text("started_at")
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP()`),
+  bookID: text("book_id")
+    .notNull()
+    .references(() => bookSchema.id),
+});
+
+export const trackRelations = relations(trackSchema, ({ one }) => ({
+  book: one(bookSchema, {
+    fields: [trackSchema.bookID],
+    references: [bookSchema.id],
+  }),
+}));
+
+export const bookRelations = relations(bookSchema, ({ many }) => ({
+  tracks: many(trackSchema),
+}));
