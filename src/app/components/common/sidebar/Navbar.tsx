e86ef08bfc7a176cc324f718f390aafd83c78d29
SantiagoZarate <santiago2001zarate@gmail.com>
2024-11-19 15:16:25 -0300

    style: show arrow animation when hovering sidebar links

--- src/app/components/common/sidebar/Navbar.tsx
+++ src/app/components/common/sidebar/Navbar.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { AddSquareMicroIcon } from "../../icons/AddSquareMicroIcon";
+import { ArrowTopRigthMicroIcon } from "../../icons/ArrowTopRigthMicroIcon";
 import { HomeMicroIcon } from "../../icons/HomeMicroIcon";
 
 const SIDEBAR_LINKS = [
@@ -28,9 +29,19 @@ export function Navbar() {
         <Link
           key={link.path}
           href={link.path}
-          className="flex gap-2 items-center sm:p-2 p-4 hover:bg-card transition"
+          className="flex gap-2 items-center sm:p-2 p-4 hover:bg-card transition group"
         >
-          <span>{link.icon}</span>
+          <section className="relative">
+            <span className="opacity-0">
+              <ArrowTopRigthMicroIcon />
+            </span>
+            <span className="absolute top-0 left-0 group-hover:-translate-y-4 transition group-hover:opacity-0">
+              {link.icon}
+            </span>
+            <span className="opacity-0 absolute group-hover:-translate-y-4 group-hover:opacity-100 transition">
+              <ArrowTopRigthMicroIcon />
+            </span>
+          </section>
           <p
             className={`hidden sm:block uppercase tracking-wider text-xs transition ${
               path === link.path ? "opacity-100" : "opacity-50"
