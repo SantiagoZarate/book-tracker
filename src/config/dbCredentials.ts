cf4f1ebe529a3f990e5a734e7405b294dd91c3ae
SantiagoZarate <santiago2001zarate@gmail.com>
2024-11-21 23:12:41 -0300

    feat: change env file depending on enviroment

--- src/config/dbCredentials.ts
+++ src/config/dbCredentials.ts
@@ -1,6 +1,16 @@
 import { Config } from '@libsql/client';
 import path from 'node:path';
+import envs from './envs';
 
-export const dbCredentials: Config = {
-  url: `file:${path.join(process.cwd(), `/src/db/local.development.db`)}`,
-};
+export let dbCredentials: Config;
+
+if (envs.mode === 'production') {
+  dbCredentials = {
+    url: envs.db.url,
+    authToken: envs.db.token,
+  };
+} else {
+  dbCredentials = {
+    url: `file:${path.join(process.cwd(), `/src/db/local.development.db`)}`,
+  };
+}
--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,6 +1,11 @@
+import { config } from 'dotenv';
 import { z } from 'zod';
 
+const path = `.env.${process.env.NODE_ENV}`;
+config({ path });
+
 const envsSchema = z.object({
+  mode: z.enum(['production', 'development', 'test']).default('development'),
   auth: z.object({
     github: z.object({
       id: z.string(),
@@ -8,17 +13,26 @@ const envsSchema = z.object({
     }),
     secret: z.string(),
   }),
+  db: z.object({
+    url: z.string(),
+    token: z.string(),
+  }),
 });
 
 type EnvsType = z.infer<typeof envsSchema>;
 
 const envs: EnvsType = {
+  mode: process.env.NODE_ENV,
   auth: {
     github: {
       id: process.env.GITHUB_CLIENT_ID ?? '',
       secret: process.env.GITHUB_CLIENT_SECRET ?? '',
     },
     secret: process.env.NEXTAUTH_SECRET ?? '',
+  },
+  db: {
+    token: process.env.TURSO_TOKEN ?? '',
+    url: process.env.TURSO_URL ?? '',
   },
 };
 
