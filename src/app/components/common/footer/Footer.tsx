044e82706012005e3f7b7696b4de0b3d1ddc8b59
SantiagoZarate <santiago2001zarate@gmail.com>
2024-12-24 19:16:56 -0300

    chore: add hyperlinks in footer

--- src/app/components/common/footer/Footer.tsx
+++ src/app/components/common/footer/Footer.tsx
@@ -2,8 +2,21 @@ export function Footer() {
   return (
     <footer className="mx-auto w-full">
       <section className="mx-auto flex max-w-screen-lg flex-col gap-2 border-t border-border pb-12 pt-4">
-        <p className="text-sm">Created by Santiago Zarate</p>
-        <p className="text-xs text-primary/50">Source code</p>
+        <p className="text-sm">
+          Created by{' '}
+          <a href="https://santiago-zarate.netlify.app/" target="_blank">
+            Santiago Zarate
+          </a>{' '}
+        </p>
+        <a
+          href="https://github.com/SantiagoZarate/book-tracker/tree/main"
+          target="_blank"
+          className="w-fit"
+        >
+          <p className="text-xs text-primary/50 hover:text-primary hover:underline">
+            Source code
+          </p>
+        </a>
       </section>
     </footer>
   );
